23863ed126a0df7f57cbbebdad5a9ed473ebe668
Javier Lete <jglete@eskura21.com>
2023-02-07 17:17:35 +0100

    Creación del proyecto almacen

---
+++ almacen/src/main/java/com/inetum/ejemplos/almacen/AlmacenApplication.java
@@ -0,0 +1,13 @@
+package com.inetum.ejemplos.almacen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmacenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmacenApplication.class, args);
+	}
+
+}
