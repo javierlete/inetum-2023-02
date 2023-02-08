81499d55dd9de2efa58827515c369af80d126be6
Javier Lete <jglete@eskura21.com>
2023-02-08 17:10:40 +0100

    CrossOrigin

--- almacen/src/main/java/com/inetum/ejemplos/almacen/rest/ProductoRestController.java
+++ almacen/src/main/java/com/inetum/ejemplos/almacen/rest/ProductoRestController.java
@@ -2,6 +2,7 @@ package com.inetum.ejemplos.almacen.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.inetum.ejemplos.almacen.daos.Dao;
 import com.inetum.ejemplos.almacen.entidades.Producto;
 import java.util.Objects;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/productos")
 public class ProductoRestController {
