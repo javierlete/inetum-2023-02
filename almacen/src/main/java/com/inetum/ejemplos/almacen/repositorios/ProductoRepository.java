c2b8707dc23a66fa7c5eab7cfadff642b0143d0a
Javier Lete <jglete@eskura21.com>
2023-02-13 16:42:20 +0100

    Spring Data Rest Repository

--- almacen/src/main/java/com/inetum/ejemplos/almacen/repositorios/ProductoRepository.java
+++ almacen/src/main/java/com/inetum/ejemplos/almacen/repositorios/ProductoRepository.java
@@ -2,10 +2,13 @@ package com.inetum.ejemplos.almacen.repositorios;
 
 import java.math.BigDecimal;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.inetum.ejemplos.almacen.entidades.Producto;
 
-public interface ProductoRepository extends CrudRepository<Producto, Long> {
-	Iterable<Producto> findByPrecioBetween(BigDecimal inferior, BigDecimal superior);
+@RepositoryRestResource(collectionResourceRel = "productos", path = "productos")
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+	Iterable<Producto> findByPrecioBetween(@Param("inferior") BigDecimal inferior, @Param("superior") BigDecimal superior);
 }
